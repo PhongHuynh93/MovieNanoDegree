4e6abf4889492e3328019d0da1c69cf0aabce9d3
Phong <p7157568@gmail.com>
2016-07-30 10:36:55 +0700

    feat - add movie response

--- app/src/main/java/dhbk/android/movienanodegree/io/model/MovieVideosResponse.java
+++ app/src/main/java/dhbk/android/movienanodegree/io/model/MovieVideosResponse.java
@@ -3,5 +3,34 @@ package dhbk.android.movienanodegree.io.model;
 /**
  * Created by huynhducthanhphong on 7/30/16.
  */
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+/**
+ * see http://docs.themoviedb.apiary.io/#reference/movies/movieidvideos/get
+ * for field in JSON response for movie video
+ *
+ * Get the videos (trailers, teasers, clips, etc...) for a specific movie id.
+ * http://api.themoviedb.org/3/movie/id/videos
+ * {
+ "id": 550,
+ "results": [
+     {
+         "id": "533ec654c3a36854480003eb",
+         "iso_639_1": "en",
+         "key": "SUXWAEX2jlg",
+         "name": "Trailer 1",
+         "site": "YouTube",
+         "size": 720,
+         "type": "Trailer"
+     }
+ ]
+ }
+ */
 public class MovieVideosResponse {
+    @SerializedName(MovieResponseConstant.MOVIE_RESULTS)
+    private ArrayList<MovieVideo> results;
+
 }
