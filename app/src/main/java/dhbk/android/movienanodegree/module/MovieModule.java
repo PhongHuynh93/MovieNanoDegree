ada2f3de9ccea5f516d69090bf741732a6954180
Phong <p7157568@gmail.com>
2016-07-31 23:41:03 +0700

    feat - implement moviecomponent module

--- app/src/main/java/dhbk/android/movienanodegree/module/MovieModule.java
+++ app/src/main/java/dhbk/android/movienanodegree/module/MovieModule.java
@@ -1,6 +1,13 @@
 package dhbk.android.movienanodegree.module;
 
+import android.content.Context;
+
 import dagger.Module;
+import dagger.Provides;
+import dhbk.android.movienanodegree.interactor.MovieInteractor;
+import dhbk.android.movienanodegree.io.MovieApiAdapter;
+import dhbk.android.movienanodegree.io.MovieApiService;
+import retrofit2.Retrofit;
 
 /**
  * Created by phongdth.ky on 7/29/2016.
@@ -8,5 +15,32 @@ import dagger.Module;
  */
 @Module
 public class MovieModule {
+    /**
+     * retrofit instance
+     * @return
+     */
+    @Provides
+    public Retrofit provideRetrofitInstance(Context context) {
+        return MovieApiAdapter.getInstance(context);
+    }
 
+    /**
+     * endpoints
+     * @param retrofit
+     * @return
+     */
+    @Provides
+    public MovieApiService provideSpotifyApiService(Retrofit retrofit) {
+        return retrofit.create(MovieApiService.class);
+    }
+
+    /**
+     * method for client to connect to api
+     * @param apiService
+     * @return
+     */
+    @Provides
+    public MovieInteractor provideArtistSearchInteractor(MovieApiService apiService) {
+        return new MovieInteractor(apiService);
+    }
 }
