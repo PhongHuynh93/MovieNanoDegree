174cace52dcee4c83c27acda5f14158f926772df
Phong <p7157568@gmail.com>
2016-08-01 14:22:54 +0700

    feat - make adapter

--- app/src/main/java/dhbk/android/movienanodegree/ui/home/adapter/ListMovieRecyclerViewAdapter.java
+++ app/src/main/java/dhbk/android/movienanodegree/ui/home/adapter/ListMovieRecyclerViewAdapter.java
@@ -5,13 +5,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import dhbk.android.movienanodegree.R;
 
 /**
  * Created by huynhducthanhphong on 8/1/16.
  */
-public class ListMovieRecyclerViewAdapter extends RecyclerView.Adapter<ListMovieRecyclerViewAdapter.MovieViewHolder>{
+public class ListMovieRecyclerViewAdapter extends RecyclerView.Adapter<ListMovieRecyclerViewAdapter.MovieViewHolder> {
     private final Context mContext;
 
     public ListMovieRecyclerViewAdapter(Context context) {
@@ -42,8 +47,23 @@ public class ListMovieRecyclerViewAdapter extends RecyclerView.Adapter<ListMovie
     }
 
     public static class MovieViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.textview_listmovie_nameofmovie)
+        TextView mTextviewListmovieNameofmovie;
+        @BindView(R.id.textview_listmovie_typeofmovie)
+        TextView mTextviewListmovieTypeofmovie;
+        @BindView(R.id.textview_listmovie_lengthofmovie)
+        TextView mTextviewListmovieLengthofmovie;
+        @BindView(R.id.textview_listmovie_descriptionofmovie)
+        TextView mTextviewListmovieDescriptionofmovie;
+        @BindView(R.id.relativelayout_listmovie_maincontent)
+        RelativeLayout mRelativelayoutListmovieMaincontent;
+        @BindView(R.id.imageview_listmovie_imageofmovie)
+        ImageView mImageviewListmovieImageofmovie;
+
         public MovieViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+
         }
     }
 }
