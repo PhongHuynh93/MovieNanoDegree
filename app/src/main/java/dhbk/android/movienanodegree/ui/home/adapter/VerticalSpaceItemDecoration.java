78bcb58121c01c2af36d5de2cac747e98917d61f
Phong <p7157568@gmail.com>
2016-08-02 07:52:07 +0700

    feat - add space to list

---
+++ app/src/main/java/dhbk/android/movienanodegree/ui/home/adapter/VerticalSpaceItemDecoration.java
@@ -0,0 +1,27 @@
+package dhbk.android.movienanodegree.ui.home.adapter;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by huynhducthanhphong on 8/2/16.
+ * add bottom space in recyclerview
+ */
+public class VerticalSpaceItemDecoration extends RecyclerView.ItemDecoration {
+
+    private final int mVerticalSpaceHeight;
+
+    public VerticalSpaceItemDecoration(int mVerticalSpaceHeight) {
+        this.mVerticalSpaceHeight = mVerticalSpaceHeight;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+                               RecyclerView.State state) {
+        // not get space at the last item in list
+        if (parent.getChildAdapterPosition(view) != parent.getAdapter().getItemCount() - 1) {
+            outRect.bottom = mVerticalSpaceHeight;
+        }
+    }
+}
