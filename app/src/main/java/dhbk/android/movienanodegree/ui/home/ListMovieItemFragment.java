8344d4a0937276651b4546e70aa0a30ab790a61a
Phong <p7157568@gmail.com>
2016-08-07 10:41:19 +0700

    feat - make mathod when called onClickItem

--- app/src/main/java/dhbk/android/movienanodegree/ui/home/ListMovieItemFragment.java
+++ app/src/main/java/dhbk/android/movienanodegree/ui/home/ListMovieItemFragment.java
@@ -19,7 +19,7 @@ import dhbk.android.movienanodegree.R;
 import dhbk.android.movienanodegree.ui.base.BaseFragment;
 import dhbk.android.movienanodegree.ui.home.adapter.EndlessRecyclerViewScrollListener;
 import dhbk.android.movienanodegree.ui.home.adapter.ListMovieRecyclerViewAdapter;
-import dhbk.android.movienanodegree.ui.home.adapter.OnItemClickListener;
+import dhbk.android.movienanodegree.ui.home.adapter.OnItemSelectedListener;
 import dhbk.android.movienanodegree.ui.home.adapter.VerticalSpaceItemDecoration;
 import dhbk.android.movienanodegree.ui.home.component.DaggerListMovieChildViewComponent;
 import dhbk.android.movienanodegree.ui.home.module.ListMovieRecyclerViewAdapterModule;
@@ -45,6 +45,8 @@ public class ListMovieItemFragment extends BaseFragment {
 
     private EndlessRecyclerViewScrollListener endlessRecyclerViewScrollListener;
     private int mTabLayoutPosition;
+    private OnItemSelectedListener onItemSelectedListener;
+
 
     public ListMovieItemFragment() {
         // Required empty public constructor
@@ -110,8 +112,9 @@ public class ListMovieItemFragment extends BaseFragment {
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
 
+        // TODO: 8/7/16 add activity call this setOnCLikMethod
         // : 8/1/16 set adapter for recyclerview
-        mListMovieRecyclerViewAdapter.setOnItemClickListener((itemView, position) -> onItemSelectedListener.onItemSelected(adapter.getItem(position)));
+        mListMovieRecyclerViewAdapter.setOnItemClickListener((itemView, position) -> onItemSelectedListener.onItemSelected(mListMovieRecyclerViewAdapter.getItem(position)));
         mRecyclerviewHomeListMovies.setAdapter(mListMovieRecyclerViewAdapter);
         // make list show 1 vertical column of data
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
