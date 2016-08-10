974cfcbcab2c442523d30b11db9635bdfe870a6e
Phong <p7157568@gmail.com>
2016-08-10 21:03:17 +0700

    feat - add fab show vote average

--- app/src/main/java/dhbk/android/movienanodegree/ui/listmovie/ListMovieRecyclerViewAdapter.java
+++ app/src/main/java/dhbk/android/movienanodegree/ui/listmovie/ListMovieRecyclerViewAdapter.java
@@ -57,8 +57,9 @@ public class ListMovieRecyclerViewAdapter extends CursorRecyclerViewAdapter<List
             DiscoverMovie movie = DiscoverMovie.fromCursor(cursor);
 
             viewHolder.mTextviewListmovieNameofmovie.setText(movie.getOriginalTitle());
-            viewHolder.mTextviewListmovieTypeofmovie.setText(movie.getOriginalTitle());
-            viewHolder.mTextviewListmovieLengthofmovie.setText(movie.getOriginalTitle());
+//            viewHolder.mTextviewListmovieTypeofmovie.setText(movie.getOriginalTitle());
+//            viewHolder.mTextviewListmovieLengthofmovie.setText(movie.getOriginalTitle());
+            viewHolder.mfabVoteAverate.setText("" + movie.getAverageVote());
             viewHolder.mTextviewListmovieDescriptionofmovie.setText(movie.getOverview());
 
             String urlImage = Constant.POSTER_IMAGE_BASE_URL + Constant.POSTER_IMAGE_SIZE + movie.getPosterPath();
@@ -90,6 +91,8 @@ public class ListMovieRecyclerViewAdapter extends CursorRecyclerViewAdapter<List
         TextView mTextviewListmovieDescriptionofmovie;
         @BindView(R.id.imageview_listmovie_imageofmovie)
         ImageView mImageviewListmovieImageofmovie;
+        @BindView(R.id.textview_listmovie_vote)
+        TextView mfabVoteAverate;
 
         public MovieViewHolder(View itemView, OnItemClickListener onItemClickListener) {
             super(itemView);
